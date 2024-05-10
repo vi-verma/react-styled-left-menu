b16269beb4257ad18e47953493868c658d2d791c
Vivek Verma <vivekverma74888@gmail.com>
2024-05-10 14:17:11 +0530

    css changes

--- src/components/chartTypeSelection/ChartSelectionDrawer.jsx
+++ src/components/chartTypeSelection/ChartSelectionDrawer.jsx
@@ -1,19 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 
-
 const ICON_PATH = "/src/assets/chartIcons/";
 
 const DrawerContainer = styled.div`
-  width: 34vw;
-  height: 96vh;
-  padding:8px;
+  width: 30vw;
+  height: 92vh;
   border-radius: 6px;
   font-family: "Lato", sans-serif;
   position: fixed;
   top: 10px;
-  right:10px;
-  overflow: auto;
+  right: 10px;
   /* right: ${({ isopen }) => (isopen ? "0" : "-300px")}; */
   background-color: white;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
@@ -25,36 +22,42 @@ const DrawerHeader = styled.div`
   display: flex;
   color: grey;
   justify-content: space-between;
-  align-items:center;
-  padding: 1rem 0.7rem 0.4rem 1rem;
+  align-items: center;
+  padding: 0.6rem;
 `;
 const HeaderText = styled.div`
-  font-size: .9rem;
+  font-size: 0.9rem;
   padding: 0 0.6rem;
 `;
 const CloseIcon = styled.img`
   padding: 0 0.4rem;
-  height: .8rem;
+  height: 0.8rem;
 `;
 const Divider = styled.hr`
   border: none;
   border-top: 1px solid #b0afaf53;
   margin: 4px 0;
 `;
-
+const ListContainer = styled.div`
+  height: 84vh;
+  width: 100%;
+  margin: 0;
+  overflow: auto;
+`;
 const List = styled.ul`
+  padding: 0 8px;
   list-style: none;
-  padding: 0;
   display: flex;
   flex-wrap: wrap;
 `;
 
 const ListItem = styled.li`
-  width: ${({ itemPerRow }) => `calc(${100 / itemPerRow}% - 8% )`};
-  height: auto;
-  padding: 2% 4% ;
-  margin: 2%;
-  font-size: .7rem;
+  width: ${({ itemPerRow }) => `calc(${100 / itemPerRow}% - 6% )`};
+  height: ${({ itemPerRow }) => `calc(${30 / itemPerRow}vw - 2vw )`};
+  padding: 0;
+  margin: 1%;
+  padding: 2% 1%;
+  font-size: 0.7rem;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -68,32 +71,31 @@ const ListItem = styled.li`
 `;
 
 const Icon = styled.img`
-  width: 90%;
-  height: 50%;
+  width: 50%;
+`;
+
+const Label = styled.div`
   padding: 2%;
 `;
 
-const Label = styled.span`
-  padding: 1%;
-`;
-
-const ChartPickerDrawer = ({ isopen, data, itemPerRow = 6 }) => {
-
+const ChartPickerDrawer = ({ isopen, data, itemPerRow = 3 }) => {
   return (
     <DrawerContainer isopen={isopen}>
       <DrawerHeader>
         <HeaderText>Choose your Chart Type</HeaderText>
-        <CloseIcon src={ICON_PATH + 'cross_icon.png'} alt="Icon" />
+        <CloseIcon src={ICON_PATH + "cross_icon.png"} alt="Icon" />
       </DrawerHeader>
       <Divider />
-      <List>
-        {data.map((item, index) => (
-          <ListItem key={index} itemPerRow={itemPerRow}>
-            {item?.icon && <Icon src={ICON_PATH + item?.icon} alt={'Icon'} />}
-            {item?.label && <Label>{item?.label}</Label>}
-          </ListItem>
-        ))}
-      </List>
+      <ListContainer>
+        <List>
+          {data.map((item, index) => (
+            <ListItem key={index} itemPerRow={itemPerRow}>
+              {item?.icon && <Icon src={ICON_PATH + item?.icon} alt={"Icon"} />}
+              {item?.label && <Label>{item?.label}</Label>}
+            </ListItem>
+          ))}
+        </List>
+      </ListContainer>
     </DrawerContainer>
   );
 };
